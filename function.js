d35ca63536753d3deb378ab2b7dd173583e5edc4
Your Name <960946031@qq.com>
2017-09-08 23:52:41 +0800

    'beta1'

---
+++ function.js
@@ -0,0 +1,36 @@
+/*
+* @Author: 靳振国
+* @Date:   2017-09-08 17:21:32
+* @Last Modified by:   靳振国
+* @Last Modified time: 2017-09-08 22:06:07
+*/
+// 连字符引入外部js
+// function join(arr,str){
+// 	var result='';
+// 		for(var i=0;i<arr.length;i++){
+// 			if(i<arr.length-1){
+// 				result += arr[i] + str;
+// 			}else{
+// 				result += arr[i];
+// 			}
+// 		}
+// 		return result;
+// }
+// 
+// 映射数值+2
+// function map(arr,fn){
+// 	var newarr=[];
+// 	for(var i=0;i<arr.length;i++){
+// 		newarr[newarr.length] = fn(arr[i]); 
+// 	}
+// 	return newarr;
+// }
+// 映射筛选偶数
+// function map(arr){
+// 	var newarr=[];
+// 	for(var i=0;i<arr.length;i++){
+// 		if(arr[i]%2==0)
+// 		newarr[newarr.length] = arr[i];
+// 	}
+// 	return newarr;
+// }
